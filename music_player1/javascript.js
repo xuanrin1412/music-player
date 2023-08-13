2741a9f2330aafbe0af5af651ce737e3467ae5f7
xuanrin1412 <114205729+xuanrin1412@users.noreply.github.com>
2023-08-13 12:10:16 +0700

    Add files via upload

---
+++ music_player1/javascript.js
@@ -0,0 +1,16 @@
+
+/**
+ * 1. render song 1-5
+ * 2. scroll top 6-13
+ * 3. play/ pause /seek 16 - 24
+ * 4. CD rotate   25 - 26
+ * 5. next/ previous  27- 30
+ * 6. random 31-33
+ * 7. next 34/ repeat 35 when end  34 35 
+ * 8. active song // next-> active  36- 38  / css color 39 40//
+ * 9.    41  42 scroll active song into view (get class have song active pull/drag it to view)
+ * 10. play song when click   43.44 - 46
+ * 11. keep song when press random or repeat song   47 - 50
+ */
+
+// tạo ra hàm $ để lựa chọn pt thay document.querySelector
